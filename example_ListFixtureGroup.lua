5a8722b88dd93d3acb697f45518fd04794240323
imhofroger <44645619+imhofroger@users.noreply.github.com>
2020-01-14 10:53:38 +0100

    Update and rename example_ListFixtureGroup.luas to example_ListFixtureGroup.lua

--- example_ListFixtureGroup.lua
+++ example_ListFixtureGroup.lua
@@ -4,31 +4,43 @@ local componentName = select(2,...);
 local signalTable   = select(3,...);
 local my_handle     = select(4,...);
 ---------------------------------------------------------------------------
+-- **************************************************
+-- **Scriptname		:
+-- **Description	: 
+-- **Author			: Roger Imhof
+-- **Github			: https://github.com/imhofroger/GMA3_LUA
+-- **Date			: 14.01.2020
+-- **************************************************
 
----------------------------------------------------------------------------
 function ListFixtureGroups()
 	local root = Root();
+	-- Store all Groups to a Table
 	local FixtureGroups=root.ShowData.DataPools.Default.Groups:Children()
 
 	return FixtureGroups
 end
----------------------------------------------------------------------------
 
----------------------------------------------------------------------------
+--
+
 local function MyMain(display_handle)
+	-- run Function
 	local FixtureGroups = ListFixtureGroups();
 	
+	-- Create a Choise for each Group in Table
 	local choise = {};
 	for k in ipairs(FixtureGroups) do
 		table.insert(choise,"'"..FixtureGroups[k].name.."'")        
 	end
 	
+	-- Setup the Messagebox
 	library.PrintSystemMonitorMessage(2);
 	local ret = PopupInput("Select Plugin Option", display_handle, choise);
+	-- Select the Choised Group with Cmd
 	Cmd("SelFix Group '"..FixtureGroups[ret+1].name.."'")
 	
 end
----------------------------------------------------------------------------
+
+--
+
 return MyMain
----------------------------------------------------------------------------
 
